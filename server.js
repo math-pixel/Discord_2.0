53e557c75aa7a3f4bdaae66ae849a372aeb0f499
Cal <huskusocial@gmail.com>
2021-11-12 16:47:39 +0100

    ?XD

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const chalk = require('chalk');
 
 const port = 3000;
 const server = http.createServer(express);
