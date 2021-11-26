243416e353c7344868ccc28266ebbab4ad83bf56
Cal <huskusocial@gmail.com>
2021-11-26 16:36:41 +0100

    xd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         wss.clients.forEach(function each(client) {
             if(client !== ws && client.readyState == WebSocket.OPEN) {
-                client.send(message.toString);
+                client.send(message.toJSON);
             }
         });
     })
