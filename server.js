ad725e7d20b0407a2a8cb1545986d8bf3d955cba
Cal <huskusocial@gmail.com>
2021-11-29 11:21:56 +0100

    x

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const port = 3000;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
+
+
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         wss.clients.forEach(function each(client) {
             if(client !== ws && client.readyState == WebSocket.OPEN) {
-                client.send(message.toJSON);
+                client.send(message);
+                console.log(message);
             }
         });
     })
