704818415f8e99517c1d40be42d5fd71900fc42e
math-pixel <59537610+math-pixel@users.noreply.github.com>
2021-12-13 00:56:17 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const chalk = require('chalk');
+//const chalk = require('chalk');
 
 const port = 3000;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
-
-
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         wss.clients.forEach(function each(client) {
             if(client !== ws && client.readyState == WebSocket.OPEN) {
-                client.send(message);
-                console.log(message);
+            	console.log(message.toString());
+                client.send(message.toString());
             }
         });
     })
